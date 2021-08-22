5f10f76b709dbe15813e43636069dea953c4f4f3
SeuNomeDeUsuario <seuMail@example.br>
2021-08-22 18:40:21 -0300

    update resume

---
+++ common_variables.R
@@ -0,0 +1,16 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Postdoctoral fellow in Federal University of Rio Grande do Sul and temporary professor at 
+    Federal University of Ceara."
+
+
+# Language skills for skill bars visualization
+
+skills <- tibble::tribble(
+  ~skill,               ~level,
+  "R",                  5,
+  "Markdown",           4.5,
+  "Git",                4,
+  "html",               3
+)
