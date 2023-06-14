2d5b84a98276007cc41b0b4040cf37802ea59de0
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2023-06-14 09:37:06 -0300

    updating cv

--- common_variables.R
+++ common_variables.R
@@ -1,7 +1,7 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "Postdoctoral Researcher at National Institute of Science and Technology/Ecology Evolution and Biodiversity conservation - Brazil"
+intro_text <- "Postdoctoral Researcher at National Institute of Science and Technology | Ecology Evolution and Biodiversity conservation - Brazil | Universiversidade Federal de Goiás"
 
 
 # Language skills for skill bars visualization
